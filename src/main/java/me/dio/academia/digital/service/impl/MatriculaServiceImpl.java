7aa050ff2e164bfc2d8b2ddf98c4a4f404791def
leofab <leofabr3@gmail.com>
2023-03-21 16:54:00 -0300

    MatriculaRepository MatriculaController & MatriculaServiceImpl implement

--- src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
@@ -1,17 +1,33 @@
 package me.dio.academia.digital.service.impl;
 
+import me.dio.academia.digital.entity.Aluno;
 import me.dio.academia.digital.entity.Matricula;
 import me.dio.academia.digital.entity.form.MatriculaForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.repository.MatriculaRepository;
 import me.dio.academia.digital.service.IMatriculaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MatriculaServiceImpl implements IMatriculaService {
+
+    @Autowired
+    private MatriculaRepository matriculaRepository;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
     @Override
     public Matricula create(MatriculaForm form) {
-        return null;
+        Matricula matricula = new Matricula();
+        Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
+
+        matricula.setAluno(aluno);
+
+        return matriculaRepository.save(matricula);
     }
 
     @Override
@@ -20,8 +36,13 @@ public class MatriculaServiceImpl implements IMatriculaService {
     }
 
     @Override
-    public List<Matricula> getAll() {
-        return null;
+    public List<Matricula> getAll(String bairro) {
+        if(bairro == null){
+            return matriculaRepository.findAll();
+        }else{
+
+        return matriculaRepository.findAlunosMatriculadosBairro(bairro);
+        }
     }
 
     @Override
