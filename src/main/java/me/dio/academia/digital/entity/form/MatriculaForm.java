0a452e0e6d26044e6e70468073c606816f0397ae
leofab <leofabr3@gmail.com>
2023-03-21 15:59:59 -0300

    MatriculaForm spring boot methods implements

--- src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -1,7 +1,19 @@
 package me.dio.academia.digital.entity.form;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MatriculaForm {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long alunoId;
 
 }
